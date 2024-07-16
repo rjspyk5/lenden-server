4c05c99b7b75cf694053645a99b43daea9450dde
rjspyk5 <rjspyk5@gmail.com>
2024-07-16 18:57:28 +0600

    unknown

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.omgilvs.mongodb.net/?appName=Cluster0`;
 
 app.use(express.json());
 app.use(cors());
@@ -12,5 +14,28 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("server is ready");
 });
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => console.log("server is running"));
