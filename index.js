979a0de75bbfa813b988665125d64ecd054d3201
rjspyk5 <rjspyk5@gmail.com>
2024-07-31 23:38:18 +0600

    api making for reg user

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.omgilvs.mongodb.net/?appName=Cluster0`;
-
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5174", "http://localhost:5173"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("server is ready");
@@ -26,13 +31,23 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
 
+    const userCollection = client.db("lenden").collection("userCollection");
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
+
+    app.post("/reg", async (req, res) => {
+      const data = req.body;
+      const query = {
+        $or: [{ email: req.body.email }, { number: req.body.number }],
+      };
+      // chekced registered already or not
+      const isAlreadyHaveAccount = userCollection.findOne(query);
+    });
   } finally {
   }
 }
