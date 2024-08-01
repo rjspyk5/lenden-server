7635dd3a20fc45c2e18cd6460caefadaba1657ab
rjspyk5 <rjspyk5@gmail.com>
2024-08-02 01:27:25 +0600

    api correction for login

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.omgilvs.mongodb.net/?appName=Cluster0`;
+
+// middlewares
 app.use(express.json());
 app.use(
   cors({
@@ -17,11 +19,7 @@ app.use(
 );
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("server is ready");
-});
-
-// middleware
+//custom middleware
 const hashedPass = async (req, res, next) => {
   const password = req.body.password;
   const hashedPassword = await bcrypt.hash(password, 10);
@@ -29,6 +27,21 @@ const hashedPass = async (req, res, next) => {
   next();
 };
 
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "Not authorized" });
+  }
+  jwt.verify(token, process.env.token, (er, decoded) => {
+    if (er) {
+      res.status(401).send({ message: "unauthorized" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
+// database connection
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -39,14 +52,27 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
-    // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-
     // collection names
     const userCollection = client.db("lenden").collection("userCollection");
 
+    const cookieOptions = {
+      httpOnly: true,
+      secqure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    };
+    // jwt related token
+    app.post("/jwt", async (req, res) => {
+      const userEmail = req.body;
+      const token = jwt.sign(userEmail, process.env.token, { expiresIn: "7d" });
+      res.cookie("token", token, cookieOptions).send({ success: true });
+    });
+    app.post("/logout", async (req, res) => {
+      res
+        .clearCookie("token", { ...cookieOptions, maxAge: 0 })
+        .send({ success: true });
+    });
     //  login logout related api
     app.post("/reg", hashedPass, async (req, res) => {
       const data = req.body;
@@ -85,9 +111,18 @@ async function run() {
             return res.send({ result: "Something Went Wrong" });
           }
           if (ress) {
-            return res.send({ result: true });
+            console.log(isAnyAccountHave);
+            return res.send({
+              result: true,
+              data: {
+                name: isAnyAccountHave.name,
+                email: isAnyAccountHave?.email,
+                photo: isAnyAccountHave.photo,
+                accountStats: isAnyAccountHave.status,
+              },
+            });
           } else {
-            res.send({ result: false });
+            res.send({ result: "Password Didn't match" });
           }
         });
       }
